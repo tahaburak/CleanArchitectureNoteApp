4ab2f0e43d2dc58f5eab2fd9aa4b0c54261840bf
tahaburak <tahaburak.koc@gmail.com>
2022-06-04 01:11:06 +0200

    created model

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature/note/domain/model/Note.kt
@@ -0,0 +1,21 @@
+package com.plcoding.cleanarchitecturenoteapp.feature.note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.*
+
+/**
+ * Created by burak on 04.06.22
+ */
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
