f2317ce4ef81b5f187d5e8750db8183cec246f19
tahaburak <tahaburak.koc@gmail.com>
2022-06-04 01:11:15 +0200

    created db

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature/note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,18 @@
+package com.plcoding.cleanarchitecturenoteapp.feature.note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.plcoding.cleanarchitecturenoteapp.feature.note.domain.model.Note
+
+/**
+ * Created by burak on 04.06.22
+ */
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+
+abstract class NoteDatabase : RoomDatabase() {
+    abstract val noteDao: NoteDao
+}
