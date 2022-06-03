9bba1e97c2b629cff2ed4cba9e32ac1a522ebb7c
tahaburak <tahaburak.koc@gmail.com>
2022-06-04 01:29:40 +0200

    created delete note use case

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature/note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cleanarchitecturenoteapp.feature.note.domain.use_case
+
+/**
+ * Created by burak on 04.06.22
+ */
+data class NoteUseCases(
+    val getNotes: GetNotes,
+    val deleteNote: DeleteNote
+)
